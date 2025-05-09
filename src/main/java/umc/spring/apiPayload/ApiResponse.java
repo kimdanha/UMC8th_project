deb416faf1791c2e22e4ef041657913f2ba6583f
kimdanha <lily034686@gmail.com>
2025-05-09 23:02:43 +0900

    feat: test api

--- src/main/java/umc/spring/apiPayload/ApiResponse.java
+++ src/main/java/umc/spring/apiPayload/ApiResponse.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import umc.spring.apiPayload.code.BaseCode;
+import umc.spring.apiPayload.code.status.SuccessStatus;
 
 @Getter
 @AllArgsConstructor
@@ -18,19 +20,14 @@ public class ApiResponse<T> {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private T result;
 
+    public static <T> ApiResponse<T> onSuccess(T result){
+        return new ApiResponse<>(true, SuccessStatus._OK.getCode() , SuccessStatus._OK.getMessage(), result);
+    }
 
-    // 성공한 경우 응답 생성
+    public static <T> ApiResponse<T> of(BaseCode code, T result){
+            return new ApiResponse<>(true, code.getReasonHttpStatus().getCode() , code.getReasonHttpStatus().getMessage(), result);
+    }
 
-//    public static <T> ApiResponse<T> onSuccess(T result){
-//        return new ApiResponse<>(true, SuccessStatus._OK.getCode() , SuccessStatus._OK.getMessage(), result);
-//    }
-//
-//    public static <T> ApiResponse<T> of(BaseCode code, T result){
-    //        return new ApiResponse<>(true, code.getReasonHttpStatus().getCode() , code.getReasonHttpStatus().getMessage(), result);
-//    }
-
-
-    // 실패한 경우 응답 생성
     public static <T> ApiResponse<T> onFailure(String code, String message, T data){
         return new ApiResponse<>(false, code, message, data);
     }
