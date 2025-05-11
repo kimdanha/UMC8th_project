b4da951fd739a52440d938a2c0d1f210ba848ebd
kimdanha <lily034686@gmail.com>
2025-05-12 02:03:17 +0900

    feature: test api

--- src/main/java/umc/spring/web/dto/TempResponse.java
+++ src/main/java/umc/spring/web/dto/TempResponse.java
@@ -14,4 +14,13 @@ public class TempResponse {
     public static class TempTestDTO{
         String testString;
     }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class TempExceptionDTO{
+        Integer flag;
+    }
+
 }
